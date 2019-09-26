bbd2e0b414c1bd63a5d79d808fc41f52d3ad71d2
koryongholee <yongho6297@naver.com>
2019-09-26 15:58:56 +0900

    commit

--- best album(RE).js
+++ best album(RE).js
@@ -3,6 +3,7 @@ function solution(genres, plays) {
     var answer = [];
     let result = {};
 
+    //객체 생성 	{ classic: { t_play: 1450, songarr: [ [Array], [Array], [Array] ] }, pop: { t_play: 3100, songarr: [ [Array], [Array] ] } }
     result  = genres.reduce((t,val,index)=>{
         if(!t[val]){
             t[val] = {t_play : plays[index] ,  songarr : []}; 
@@ -14,10 +15,12 @@ function solution(genres, plays) {
         return t;
     },{});
 
+    //t_play 내림차순 정렬
     let sorted_result = Object.values(result).sort(function(a,b){
         return b.t_play - a.t_play;
     });
 
+    //각 songarr 정렬 후 2개 or 1개 answer.push
     sorted_result.forEach((val)=>{
         if(val.songarr.length > 1){
             val.songarr.sort((a,b)=>{
