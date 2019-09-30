fa92cdd0bc52c6a1cef1001a75af0954aa2577a2
koryongholee <yongho6297@naver.com>
2019-10-01 08:19:40 +0900

    commit

---
+++ functiondevelop.js
@@ -0,0 +1,35 @@
+function solution(progresses, speeds) {
+    var answer = [];
+    let temp_arr = [];
+    progresses.forEach((v,i)=>{
+        let val = Math.ceil((100 - progresses[i]) / speeds[i]);
+        temp_arr.push(val);
+    });
+    let num;
+    let sum=0;
+
+    temp_arr.forEach((v,i)=>{
+        if(i==0){
+            num = v;  
+            sum=1;
+        }else{
+            if(v < num){
+                 sum++;
+            }else if(v==num){
+                sum++;                                
+                if(i==(temp_arr.length-1)){
+                    answer.push(sum);
+                    num = v;
+                    sum = 1;
+                    return answer;
+                } 
+            }else{
+                answer.push(sum);
+                num = v;
+                sum=1;
+            }
+            if(i==(temp_arr.length-1)) answer.push(sum);
+        } 
+    });
+    return answer;
+}
