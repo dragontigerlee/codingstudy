a40d1d670280cde3d8058cbfa3f8bd2a6b013cd8
koryongholee <yongho6297@naver.com>
2019-10-21 09:23:51 +0900

    commit

---
+++ makeBignum.js
@@ -0,0 +1,27 @@
+function solution(number, k) {
+    var answer = '';
+    
+    let arr = number.split('');
+    let maxIndex=0;
+    let maxNum=0;
+    
+    arr.forEach((v,i)=>{
+        if(i <  number.length-k){
+             if(v > maxNum){
+                maxIndex=i;
+                maxNum = v;
+            }
+        } 
+    });
+    
+    let restNum = k - maxIndex;
+    console.log(maxIndex);
+    
+    let filteredArr = arr.filter((v,i)=>{
+        return i >= maxIndex; 
+    });
+    console.log(filteredArr);
+    
+    return answer;
+    
+}
