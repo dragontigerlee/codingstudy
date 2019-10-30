337e113f68b383bac86d02e8833d7ed1c89b33d0
koryongholee <yongho6297@naver.com>
2019-10-30 09:43:07 +0900

    c

--- joystick.js
+++ joystick.js
@@ -1,3 +1,32 @@
+function check_a(name,chk){
+    let val=0;
+    if(chk===1){
+         for(let i=1; i<name.length; i++){
+            if(name[i]=="A"){
+                val++
+            }else{
+                return val;
+            }
+        }
+    }else{
+         for(let i=name.length-1; i>=1; i--){
+            if(name[i]=="A"){
+                val++
+            }else{
+                return val;
+            }
+        }
+    }
+}
+
+function check_num(val){
+    let arr = [-1,90,89,88,87,86,85,84,83,82,81,80,79,78];    
+    for(let i=0; i<arr.length; i++){
+        if(arr[i]==val){
+            return i;
+        }
+    }
+}
 function solution(name) {
     var answer = 0;
     let middle = Math.ceil(name.length/2);
@@ -14,30 +43,40 @@ function solution(name) {
     // console.log(right_A);
     
     //첫글자(동일)
-    answer = answer + (name[0].charCodeAt(0) - 65);
-     
+    if((name[0].charCodeAt(0)-64) <=13) {
+        answer = answer + (name[0].charCodeAt(0) - 64) -1;     
+    }else{
+        let first_num = check_num(name[0].charCodeAt(0));
+        answer = answer + first_num;
+    }
     //정방향
     if((left_A == right_A) || (left_A < right_A)) {
-        //A~M
-        if(name[i].charCodeAt(0)-64 <=13){
-            
-        //N~Z
-        }else{
-            
-        }
-        
         for(let i=1; i<name.length; i++){
-         answer = answer + (name[i].charCodeAt(0) - 65);   
+            if((name[i].charCodeAt(0)-64) <=13) {
+                answer = answer + (name[i].charCodeAt(0) - 64) -1;          
+            }else{
+                let temp = check_num(name[i].charCodeAt(0));
+                answer = answer + temp;
+            }
+        }
+        answer = answer + (name.length-1);
+        if(name[name.length-1]=='A'){
+            answer= answer - check_a(name,2);
         }
         return answer;
     //역방향
     }else{
         for(let i=name.length-1; i>=1; i--){
-            if(name[i]=='A'){
-                answer++;
+            if((name[i].charCodeAt(0)-64) <=13) {
+                answer = answer + (name[i].charCodeAt(0) - 64) -1;          
             }else{
-                answer = answer + (name[i].charCodeAt(0) - 65);    
+                let temp = check_num(name[i].charCodeAt(0));
+                answer = answer + temp;
             }
+        }
+        answer = answer + (name.length-1);
+        if(name[1]=='A'){
+            answer= answer - check_a(name,1);
         }
         return answer;
     }
