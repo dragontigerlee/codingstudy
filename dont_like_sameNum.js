5d3da5ae3c5e977de35f2e051da1ed9e904ee85c
yongholee <yongholee@yongholeeui-MacBookAir.local>
2019-12-01 14:03:07 +0900

    m
    ;
    
    sdf

--- dont_like_sameNum.js
+++ dont_like_sameNum.js
@@ -11,3 +11,17 @@ function solution(arr)
     
     return answer;
 }
+
+function solution(arr)
+{
+    let temp = -1;
+    let n_arr = [];
+    n_arr =arr.filter((v,i)=>{
+       if(temp != v){
+           temp = v;
+           return true; 
+       }
+    });
+    
+    return n_arr;
+}
