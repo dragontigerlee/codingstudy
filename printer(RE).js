841e5612ef9c129aa9cf0dbe78582185bb7df4c9
yongholee <yongholee@yongholeeui-MacBookAir.local>
2019-12-01 17:28:54 +0900

    m

---
+++ printer(RE).js
@@ -0,0 +1,33 @@
+function solution(arr, location) {
+    let target = location;
+    let num ;
+    let max;
+    let answer = 1;
+    
+    while(arr.length > 0){
+        //젤 앞자리 추출
+        num = arr.shift();
+        max = Math.max.apply(null, arr);
+        //현재값보다 중요도 높은게 있으면 맨 뒤로 보내버림
+        if(max > num){
+            arr.push(num);
+            if(target===0){
+               target = arr.length-1;
+            }else{
+                target--;
+            }
+        }else{
+            if(target===0){
+                break;
+            }else{
+                answer++;
+                if(target===0){
+                   target = arr.length-1;
+                }else{
+                    target--;
+                }
+            }
+        }    
+    }
+    return answer;
+}
--- printer.js
+++ printer.js
@@ -40,3 +40,38 @@ function solution(arr, location) {
     
 }
 
+//
+function solution(priorities, location) {    
+    let mapping = priorities.map((v, i) => {
+        return {'origin': i, 'value': v};
+    });
+       
+    let target;
+    let print = [];
+    
+    // 대기목록 세팅
+    while (true) {
+        
+        // 대기목록에 대상이 없으면 while문 종료
+        if (mapping.length === 0) break;
+        
+         target = mapping[0]; // 인쇄대상(리스트 가장 앞에 있는 문서)
+        
+        // 대기목록에 target보다 높은 우선순위가 없다면 인쇄
+        if (mapping.find(v => target.value < v.value) === undefined) {
+            print.push(target);
+            mapping.shift();
+            continue;
+        };
+        
+        // 맨 앞에 원소 꺼내서 뒤로 넣기
+        mapping.shift(); 
+        mapping.push(target);
+    }
+    
+
+    for (let item in print) {
+        if (print[item].origin === location) 
+            return item * 1 + 1; //i는 index이므로 인쇄순서를 알기 위해서는 +1을 해줘야 함
+    }
+}
