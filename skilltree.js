8c881a0ea15aa8522f789cd45c196558267c6f45
yongholee <yongholee@yongholeeui-MacBookAir.local>
2020-01-27 20:47:53 +0900

    commit

--- skilltree.js
+++ skilltree.js
@@ -1,4 +1,40 @@
 function solution(skill, skill_trees) {
-    var answer = 0;
+    let answer = 0;
+    let skill_idx = -1;
+    let return_val ="T";
+    let next_step="F";
+    
+    skill_trees.forEach((v,i)=>{
+        for(let k=0; k<v.length; k++) {//skill_trees arr
+            for(let j=0; j<skill.length; j++) {//skill arr
+                if(skill.charAt(j) === v.charAt(k)) {
+                    if(skill_idx===-1){
+                        if(v.charAt(k) != skill.charAt(0)){
+                            //console.log("here//"+v+k+j);
+                            next_step="T";
+                            return_val="F";
+                            break;
+                        }
+                    }
+                    if(j < skill_idx) {
+                        next_step="T";
+                        return_val="F";
+                        break;
+                    }else {
+                        skill_idx=j;
+                        return_val="T";
+                    }   
+                }
+            }
+            if(next_step ==="T") break;
+        }
+        skill_idx=-1;
+        next_step="F";
+        if(return_val==="T"){
+            answer++;  
+        }
+        return_val="T";
+    });
+    
     return answer;
 }
